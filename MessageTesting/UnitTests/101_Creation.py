4ef96724e195ec9970a5ae14280050d971b2a846
Sulles <suleymanbarthesukhera@gmail.com>
2019-03-31 13:50:10 -0400

    Added uniqueness verification for unit test 101

--- MessageTesting/UnitTests/101_Creation.py
+++ MessageTesting/UnitTests/101_Creation.py
@@ -4,9 +4,9 @@ Updated:
 
 @author: Suleyman Barthe-Sukhera
 
-This Unit Test is meant to create multiple messages and ensure no data overlaps occur
-
-TODO: Look into NUMPY array allocation with objects in array
+This Unit Test will...
+1. Determine how long it takes to instantiate n messages
+2. Ensure each message created is unique (should not be a problem, but best to be sure)
 """
 
 import sys
@@ -19,7 +19,7 @@ msg_fields = {'id': 'return_data',
 
 try:
     n = 1000
-    print("(*) Creating " + str(n) + " messages took...")
+    print("(1) Creating " + str(n) + " messages took...")
     start_time = time()
     messages = []
     for _ in range(n):
@@ -28,9 +28,16 @@ try:
     end_time = (time()-start_time)*1000
     print(str(end_time) + " ms -OR- " + str(int(end_time*10000/32)/100) + "% of the budget")
 
-    # TODO: Go through all messages and ensure they are all unique
+    # doing string compare to ensure uniqueness, so need to get string version of each message first
+    message_strings = []
+    for m in messages:
+        message_strings.append(m.__str__())
+    for x in range(1,len(messages)):
+        if message_strings[x-1] in message_strings[x:]:
+            print("ERROR 101: message " + str(x) + " not unique!")
+    print("(2) Uniqueness verified")
 
-    print("UNIT TEST 001 SUCCESSFUL")
+    print("UNIT TEST 101 SUCCESSFUL")
 
 except Exception as e:
     print("***** EXCEPTION RAISED: " + str(e))
